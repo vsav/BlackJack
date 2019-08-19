02ed4160eccd15928b2399a801caf7e026b15525
Vadim Savostin <vsavdev@gmail.com>
2019-08-19 19:02:21 +0300

    new deck refactoring

--- deck.rb
+++ deck.rb
@@ -2,20 +2,37 @@
 # \u{2665} - heart
 # \u{2666} - diamond
 # \u{2663} - club
-  @deck = []
-  @cards = {}
-  faces = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
-            '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }
-  suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
+class Deck
+  attr_reader :deck
+  def initialize
+    @deck = []
+    combine_deck
+    deck_shuffle
+  end
 
-  suits.each do |suit|
-    faces.each do |face, value|
-      face += suit
-      @cards[face] = value
+  def combine_deck
+    cards = {}
+    faces = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
+              '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }
+    suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
+
+    suits.each do |suit|
+      faces.each do |face, value|
+        face += suit
+        cards[face] = value
+      end
+    end
+
+    cards.each do |card|
+      @deck << card
     end
   end
 
-  @cards.each do |card|
-    @deck << card
+  def deck_shuffle
+    @deck.shuffle!
   end
 
+  def take_card
+    @deck.slice!(-1)
+  end
+end

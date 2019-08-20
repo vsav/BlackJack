8b593889543a6209c3e1268bb2aa24458b7a888e
Vadim Savostin <vsavdev@gmail.com>
2019-08-20 13:51:52 +0300

    delete all but main menu

--- main.rb
+++ main.rb
@@ -4,24 +4,5 @@ require_relative './player.rb'
 require_relative './dealer.rb'
 require_relative './menu.rb'
 include Menu
-#@deck = Deck.new
-#@deck1 = Deck1.new
-#@player = Player.new
-#@dealer = Dealer.new
 
-system 'clear'
 main_menu
-#round_state
-#check_victory
-
-#puts @deck
-#puts @player
-#@player.take_card(@deck)
-#@player.take_card(@deck)
-#@player.take_card(@deck)
-#@player.hand = { "A\u2660" => 11, "A\u2663" => 11, "A\u2665" => 11 }
-#puts @player.hand
-#@dealer.take_card(@deck)
-#@dealer.take_card(@deck)
-#@dealer.take_card(@deck)
-#puts @dealer.hand

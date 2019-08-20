416a1234037f301f0ccdeeb35ea75b66180ea962
Vadim Savostin <vsavdev@gmail.com>
2019-08-20 11:39:31 +0300

    change dealers limit check

--- dealer.rb
+++ dealer.rb
@@ -1,11 +1,12 @@
 class Dealer < Person
   def initialize
     super
+    @name = 'Dealer'
   end
 
-  def limit?
+  def another_card?
     count_points
-    if @points > 21
+    if @points >= 21
       false
     elsif @points >= 17 && @points <= 21
       false
@@ -14,7 +15,10 @@ class Dealer < Person
     end
   end
 
-  def draw_hand
+  def draw_hand_hidden
+    puts "#{@name}'s hand: "
+    puts '*' * 30
     print '| ? | ' * @hand.size
+    puts
   end
 end

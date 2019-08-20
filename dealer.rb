80183266a930d2c1eed45758969b57fd30110d9b
Vadim Savostin <vsavdev@gmail.com>
2019-08-20 10:21:54 +0300

    add limit check and draw hand methods

--- dealer.rb
+++ dealer.rb
@@ -1,8 +1,20 @@
 class Dealer < Person
-  attr_reader :dealer_hand
   def initialize
     super
   end
 
+  def limit?
+    count_points
+    if @points > 21
+      false
+    elsif @points >= 17 && @points <= 21
+      false
+    else
+      true
+    end
+  end
 
+  def draw_hand
+    print '| ? | ' * @hand.size
+  end
 end

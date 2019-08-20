15aeca026264863afc958bd4b4f51f3dd1906f5f
Vadim Savostin <vsavdev@gmail.com>
2019-08-20 11:40:06 +0300

    add basic menu

--- main.rb
+++ main.rb
@@ -2,12 +2,27 @@ require_relative './deck.rb'
 require_relative './person.rb'
 require_relative './player.rb'
 require_relative './dealer.rb'
+require_relative './menu.rb'
+include Menu
+#@deck = Deck.new
+#@deck1 = Deck1.new
+#@player = Player.new
+#@dealer = Dealer.new
 
-@deck = Deck.new
-@player = Player.new
-puts @deck
-puts @player
-@player.take_card(@deck)
-@player.take_card(@deck)
-@player.take_card(@deck)
-@player.hand
+main_menu
+@player.take_card(@deck) if @player.another_card?
+@dealer.take_card(@deck) if @dealer.another_card?
+round_state
+
+
+#puts @deck
+#puts @player
+#@player.take_card(@deck)
+#@player.take_card(@deck)
+#@player.take_card(@deck)
+#@player.hand = { "A\u2660" => 11, "A\u2663" => 11, "A\u2665" => 11 }
+#puts @player.hand
+#@dealer.take_card(@deck)
+#@dealer.take_card(@deck)
+#@dealer.take_card(@deck)
+#puts @dealer.hand

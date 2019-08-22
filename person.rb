31ac895c6a97ff969a8e36d3c519732b4872a724
Vadim Savostin <vsavdev@gmail.com>
2019-08-22 18:18:59 +0300

    change hand class from hash to array, add draw_balance method

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :balance, :hand
+  attr_accessor :balance, :hand, :card
   attr_reader :name, :points, :player, :dealer, :deck
 
   def initialize(*)
@@ -8,15 +8,15 @@ class Person
   end
 
   def take_card(deck)
-    card = deck.take_card
+    @card = deck.take_card
+    @hand << card
     if limit?
-      if card.face.to_s.include?('A')
-        card.value = 1
+      if @card.face.to_s.include?('A')
+        @card.value = 1
       else
         ace_recount
       end
     end
-    @hand << card
     count_points
   end
 
@@ -36,26 +36,18 @@ class Person
   def draw_hand
     puts "#{@name}'s hand: #{@points} points total"
     puts '*' * 30
-    @hand.each_key do |card|
-      print "|#{card}| "
+    @hand.each do |card|
+      print "|#{card.face}| "
     end
     puts
+  end
+
+  def draw_balance
+    puts "#{@name}'s balance: #{@balance}"
   end
 
   def limit?
     count_points
     @points > 21
-  end
-
-  def another_card?
-    begin
-      print 'One more card?(1:take card, 2:pass): '
-      user_input = gets.to_i
-      raise 'Please input 1 to take card or 2 to pass' unless [1, 2].include?(user_input)
-    rescue RuntimeError => e
-      puts e.message
-      retry
-    end
-    return true if user_input == 1
   end
 end

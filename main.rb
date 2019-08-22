62c76f8a907dce7cd99b2e6f974d025668795a4a
Vadim Savostin <vsavdev@gmail.com>
2019-08-22 18:20:36 +0300

    main initialize

--- main.rb
+++ main.rb
@@ -1,8 +1,14 @@
+require_relative './card.rb'
 require_relative './deck.rb'
 require_relative './person.rb'
 require_relative './player.rb'
 require_relative './dealer.rb'
 require_relative './game.rb'
+require_relative './interface.rb'
 
-game = Game.new
-game.main_menu
+@deck = Deck.new
+@player = Player.new('Player1')
+@dealer = Dealer.new
+@game = Game.new(@player, @dealer, @deck)
+@interface = Interface.new(@game)
+@interface.main_menu

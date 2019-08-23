ac5a38457395a640614420bf5f64921f837fc13c
Vadim Savostin <vsavdev@gmail.com>
2019-08-23 12:20:19 +0300

    replace suits with Card class constants

--- deck.rb
+++ deck.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# \u{2660} - spades
-# \u{2665} - heart
-# \u{2666} - diamond
-# \u{2663} - club
 # playing cards deck class
 class Deck
   attr_reader :cards
@@ -16,7 +12,7 @@ class Deck
   def collect_deck
     faces = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
               '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }
-    suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
+    suits = [Card::SPADES, Card::DIAMOND, Card::CLUB, Card::HEART]
 
     suits.each do |suit|
       faces.each do |face, value|

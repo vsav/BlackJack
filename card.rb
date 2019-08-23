03bf1bad48b80f9b1cb5e032d054522f3991cf7e
Vadim Savostin <vsavdev@gmail.com>
2019-08-23 12:19:16 +0300

    add card suits constants

--- card.rb
+++ card.rb
@@ -5,6 +5,11 @@ class Card
   attr_reader :face
   attr_accessor :value
 
+  SPADES = "\u2660"
+  HEART = "\u2665"
+  DIAMOND = "\u2666"
+  CLUB = "\u2663"
+
   def initialize(face, value)
     @face = face
     @value = value

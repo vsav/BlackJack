7bb89220fed808a320a35932f2a62279599a98ef
Vadim Savostin <vsavdev@gmail.com>
2019-08-23 13:45:15 +0300

    using Card constants to collect_deck method

--- deck.rb
+++ deck.rb
@@ -10,9 +10,8 @@ class Deck
   end
 
   def collect_deck
-    faces = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
-              '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }
-    suits = [Card::SPADES, Card::DIAMOND, Card::CLUB, Card::HEART]
+    faces = Card::FACES
+    suits = Card::SUITS
 
     suits.each do |suit|
       faces.each do |face, value|

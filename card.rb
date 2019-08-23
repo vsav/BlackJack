c633cc7c37d143356cb71619f3f87a7f8f00d93b
Vadim Savostin <vsavdev@gmail.com>
2019-08-23 13:44:42 +0300

    add FACES and SUITS constants

--- card.rb
+++ card.rb
@@ -10,6 +10,10 @@ class Card
   DIAMOND = "\u2666"
   CLUB = "\u2663"
 
+  SUITS = [SPADES, HEART, DIAMOND, CLUB].freeze
+  FACES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
+            '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }.freeze
+
   def initialize(face, value)
     @face = face
     @value = value
